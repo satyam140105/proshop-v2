2ae6bcb364f996155b63f76d72026b65ceceecd6
Satyam Swain <satyamswain227@gmail.com>
2024-09-26 22:48:26 +0530

    mongoose+postman done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,22 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
-
+import logo from '../assets/logo.png';
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='ProShop' className='logo' /> ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link href='/login'>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link href='/cart'>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaUser /> Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -30,5 +24,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;

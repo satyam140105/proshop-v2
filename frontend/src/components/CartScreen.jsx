94babcd4a6c2aafa0aed919d6006f9ef2a21afdd
Satyam Swain <satyamswain227@gmail.com>
2024-09-27 23:16:52 +0530

    checkoutHandler

--- frontend/src/components/CartScreen.jsx
+++ frontend/src/components/CartScreen.jsx
@@ -23,6 +23,9 @@ const CartScreen = () => {
   const removeFromCartHandler = (id) => {
     dispatch(removeFromCart(id));
   };
+  const checkoutHandler = () => {
+    navigate('/login?redirect=shipping');
+  };
   return (
     <Row>
       <Col md={8}>
@@ -91,6 +94,7 @@ const CartScreen = () => {
                 type='button'
                 className='btn-block'
                 disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>

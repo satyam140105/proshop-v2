c7a6a267bff16e604ef23cecaec5ac7053f71f70
Satyam Swain <satyamswain227@gmail.com>
2024-09-27 21:47:02 +0530

    item count in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import {useSelector} from 'react-redux';
 const Header = () => {
+  const{cartItems}=useSelector(state=>state.cart)
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -13,6 +15,13 @@ const Header = () => {
             <Nav className='ms-auto'>
               <Nav.Link href='/cart'>
                 <FaShoppingCart /> Cart
+                {
+                  cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )
+                }
               </Nav.Link>
               <Nav.Link href='/login'>
                 <FaUser /> Sign In

2212e67055d06179f287c3434eb07d3ba689beeb
Satyam Swain <satyamswain227@gmail.com>
2024-10-01 22:53:56 +0530

    OrderPage done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {
-createBrowserRouter,
-createRoutesFromElements,
-Route,
-RouterProvider,
-} from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import PaymentScreen from './screens/PaymentScreen';
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
-
-import store from './store';
-import { Provider } from 'react-redux';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
-createRoutesFromElements(
-<Route path='/' element={<App />}>
-<Route index={true} path='/' element={<HomeScreen />} />
-<Route path='/product/:id' element={<ProductScreen />} />
-<Route path='/cart' element={<CartScreen />} />
-<Route path='/login' element={<LoginScreen />} />
-<Route path='/register' element={<RegisterScreen />} />
-{/* Registered users */}
-<Route path='' element={<PrivateRoute />}>
-<Route path='/shipping' element={<ShippingScreen />} />
-<Route path='/payment' element={<PaymentScreen />} />
-<Route path='/placeorder' element={<PlaceOrderScreen />} />
-        <Route path='/order/:id' element={<PlaceOrderScreen />} />
-</Route>
-</Route>
-)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      {/* Registered users */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+      </Route>
+    </Route>
+  )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<React.StrictMode>
-<Provider store={store}>
-<RouterProvider router={router} />
-</Provider>
-</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();

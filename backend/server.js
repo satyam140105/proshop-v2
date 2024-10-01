1b5c2d22e50f685f6db890373d25118f89321302
Satyam Swain <satyamswain227@gmail.com>
2024-10-01 23:33:44 +0530

    PAYPAL sandbox

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import {errorHandler, notFound} from './middleware/errorMiddleware.js';
-import cookieParser from 'cookie-parser';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 const port = process.env.PORT || 5000;
 connectDB(); // connect to db
 
@@ -15,17 +15,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
-app.use(cookieParser())
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-app.use('/api/users', userRoutes)       
-app.use('/api/products', productRoutes)
-app.use('/api/orders', orderRoutes)
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => 
-    console.log(`Server is running on port ${port}`)
-) 
+app.listen(port, () => console.log(`Server is running on port ${port}`));

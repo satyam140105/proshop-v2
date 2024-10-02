e9d365382bdc3beeec6c21d88755a80d48483a96
Satyam Swain <satyamswain227@gmail.com>
2024-10-02 16:16:42 +0530

    Checkout process done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,6 +22,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import store from "./store";
+import ProfileScreen from "./screens/ProfileScreen";
 import { Provider } from "react-redux";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
@@ -39,6 +40,7 @@ const router = createBrowserRouter(
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
       </Route>
     </Route>
   )

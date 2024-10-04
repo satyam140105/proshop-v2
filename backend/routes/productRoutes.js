83bdbaf4a15590fab73d9c4ae30b1b4d2453c533
Satyam Swain <satyamswain227@gmail.com>
2024-10-04 10:35:22 +0530

    admin create product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import { getProducts, getProductById } from '../controllers/productController.js';
 const router = express.Router();
-router.route('/').get(getProducts);
+import {
+getProducts,
+getProductById,
+  createProduct,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
-export default router
+
+export default router;
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,19 +1,32 @@
-import { PRODUCTS_URL } from "../constants";
-import { apiSlice } from "./apiSlice";
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: () => ({
-                url:PRODUCTS_URL
-            }),
-            keepUnusedDataFor: 5
-        }),
-        getProductDetails: builder.query({
-            query: (productId) => ({
-                url:`${PRODUCTS_URL}/${productId}`
-            }),
-            keepUnusedDataFor: 5
-        })
-    })
-})
-export const {useGetProductsQuery, useGetProductDetailsQuery} = productsApiSlice;
+endpoints: (builder) => ({
+getProducts: builder.query({
+query: () => ({
+url: PRODUCTS_URL,
+}),
+keepUnusedDataFor: 5,
+}),
+getProductDetails: builder.query({
+query: (productId) => ({
+url: `${PRODUCTS_URL}/${productId}`,
+}),
+keepUnusedDataFor: 5,
+}),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+}),
+});
+
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+} = productsApiSlice;
